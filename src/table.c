febabc3a8e8dbd0804c903adb23660d619a4ea8d
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-03 17:55:18 +0100

    add verif if allocation was successful in method

--- src/table.c
+++ src/table.c
@@ -7,6 +7,12 @@
 //Function to create a new table and initialize its parameters
 Table* create_table(){
     Table* table = (Table*)malloc(sizeof(Table));
+    
+    if (!table) {
+        fprintf(stderr, "Memory allocation failed for new node\n");
+        return;
+    }
+    
     if (table) {
         table->root = NULL;
         table->row_count = 0;
@@ -17,6 +23,12 @@ Table* create_table(){
 // Function to insert a new row into the table
 void insert_row(Table* table, Row* row) {
     Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (!newNode) {
+        fprintf(stderr, "Memory allocation failed for new node\n");
+        return;
+    }
+
     newNode->data = row;
     newNode->left = NULL;
     newNode->right = NULL;
