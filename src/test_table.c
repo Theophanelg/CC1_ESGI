df96d19e1fa44bc4822881d9bd5597524f8049ef
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-04 21:21:39 +0100

    add test_create_table method

--- src/test_table.c
+++ src/test_table.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 #include "table.h"
+
+// Test for create table 
+void test_create_table() {
+    Table* table = create_table();
+    assert(table != NULL);
+    assert(table->root == NULL);
+    assert(table->row_count == 0);
+    free_table(table);
+    printf("test_create_table is passed\n");
+}
+
+int main() {
+    test_create_table();
+    return 0;
+}
