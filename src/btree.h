a5285df4341fa2eb294d9114e17afda07a73f76d
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-04 21:18:56 +0100

    move struct node in table.h

--- src/btree.h
+++ src/btree.h
@@ -2,18 +2,10 @@
 #define BINARY_TREE_H
 #include "table.h"
 
-// Node structure representing each element in the binary tree
-typedef struct Node {
-    void* data;     
-    struct Node* left;     
-    struct Node* right;
-} Node;
-
 //call methodes
 void insert_into_binary_tree(Node* root, Node* newNode);
 Row* find_row_in_tree(Node* root, int id);
 Node* delete_node(Node* root, int id);
 void free_binary_tree(Node* root);
-
 
 #endif
