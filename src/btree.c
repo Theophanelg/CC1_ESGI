27b182a4c6c6a3447a58b70ccebb5b1fe9b6ab43
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-04 20:39:38 +0100

    Move find_row_in_tree in btree.c

--- src/btree.c
+++ src/btree.c
@@ -1,9 +1,10 @@
-// 
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include "binary_tree.h"
 #include "table.h"
 
+// Function to insert a new node into the binary tree
 void insert_into_binary_tree(Node* root, Node* newNode) {
     Row* rootData = (Row*)root->data;
     Row* newNodeData = (Row*)newNode->data;
@@ -22,3 +23,20 @@ void insert_into_binary_tree(Node* root, Node* newNode) {
         }
     }
 }
+
+// Function to find a node containing a Row with a specific ID in the tree
+Row* find_row_in_tree(Node* root, int id) {
+    if (root == NULL) {
+        return NULL;
+    }
+    Row* rowData = (Row*)root->data;
+    if (id == rowData->id) {
+        return rowData;
+    } else if (id < rowData->id) {
+        return find_row_in_tree(root->left, id);
+    } else {
+        return find_row_in_tree(root->right, id);
+    }
+}
+
+
