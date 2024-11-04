76bace45028923a2c0bba68af422b6eba6f8e593
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-04 20:48:15 +0100

    remove struct node and call method

--- src/table.h
+++ src/table.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "btree.h"
 
-//Structure to represent a row in table
+// Structure to represent a row in table
 typedef struct {
     int id;
     char name[50];
@@ -12,13 +12,18 @@ typedef struct {
     char school[50];
 } Row; 
 
-//Structure to represent a table, which could contain a binary tree of rows
-typedef struct Node Node;
-
 typedef struct {
     Node* root;
     size_t row_count;
 } Table;
+
+// Call methodes
+Table* create_table();
+Row* create_row(int id, const char* name, int age, const char* school);
+void insert_row(Table* table, Row* row);
+Row* select_row(Table* table, int id);
+void delete_row(Table* table, int id);
+void free_table(Table* table);
 
 #endif
 
