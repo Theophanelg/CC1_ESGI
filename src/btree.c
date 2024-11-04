38386a814d9c75dbc836506c1a76772fdb23ae52
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-04 20:29:28 +0100

    Update btree.c

--- src/btree.c
+++ src/btree.c
@@ -1,1 +1,24 @@
-// This module manages a binary tree data structure used to store table rows, providing efficient operations for searching, inserting, deleting, and balancing data.
+// 
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_tree.h"
+#include "table.h"
+
+void insert_into_binary_tree(Node* root, Node* newNode) {
+    Row* rootData = (Row*)root->data;
+    Row* newNodeData = (Row*)newNode->data;
+
+    if (newNodeData->id < rootData->id) {
+        if (root->left == NULL) {
+            root->left = newNode;
+        } else {
+            insert_into_binary_tree(root->left, newNode);
+        }
+    } else {
+        if (root->right == NULL) {
+            root->right = newNode;
+        } else {
+            insert_into_binary_tree(root->right, newNode);
+        }
+    }
+}
