3ed792128ada208d30b0dc3e50d81492f1b985f3
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-05 19:38:58 +0100

    add test_create_row function

--- src/test_table.c
+++ src/test_table.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "table.h"
 
@@ -12,7 +14,26 @@ void test_create_table() {
     printf("test_create_table is passed\n");
 }
 
+// Test for create row
+void test_create_row() {
+    int id = 1;
+    const char* name = "Théophane";
+    int age = 21;
+    const char* school = "ESGI";
+
+    Row* row = create_row(id, name, age, school);
+    assert(row != NULL);
+    assert(row->id == id);
+    assert(strcmp(row->name, name) == 0);
+    assert(row->age == age);
+    assert(strcmp(row->school, school) == 0);
+
+    free(row);
+    printf("test_create_row is passed\n");
+}
+
 int main() {
     test_create_table();
+    test_create_row();
     return 0;
 }
