642e0623c7d08738a5c4e026492febaed39930bc
Legrand Théophane <theophane.legrand59@gmail.com>
2024-11-16 15:05:26 +0100

    update btree, add new methodes call (print_node)

--- include/btree.h
+++ include/btree.h
@@ -3,9 +3,10 @@
 #include "../include/table.h"
 
 //call methodes
-void insert_into_binary_tree(Node* root, Node* newNode);
+void insert_into_binary_tree(Node** root, Node* newNode);
 Row* find_row_in_tree(Node* root, int id);
 Node* delete_node(Node* root, int id);
+void print_node(Node* node);
 void free_binary_tree(Node* root);
 
 #endif
