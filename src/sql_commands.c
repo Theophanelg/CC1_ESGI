1ea6f70820c118342122ec61236eefb5a08522a4
Théophane <98404020+Theophanelg@users.noreply.github.com>
2024-11-18 21:22:33 +0100

    Delete src/sql_commands.c

--- src/sql_commands.c
+++
@@ -1,3 +0,0 @@
-// This module provides functions to execute basic SQL-like commands (INSERT, SELECT, DELETE) on tables within the database, abstracting the command logic for use by other parts of the program.
-
-
